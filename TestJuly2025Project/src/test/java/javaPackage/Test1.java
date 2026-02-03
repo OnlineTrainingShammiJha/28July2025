2cb7e3b0d699bb2ca01cecc6e8f9916a562f6b49
onlinetrainingsj@gmail.com <onlinetrainingsj@gmail.com>
2026-02-03 20:45:57 +0530

    test jenkins

--- TestJuly2025Project/src/test/java/javaPackage/Test1.java
+++ TestJuly2025Project/src/test/java/javaPackage/Test1.java
@@ -1,18 +1,14 @@
 package javaPackage;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Test1 {
 
 	@Test
 	public void a() {
-		WebDriverManager.edgedriver().setup();
-		WebDriver driver = new EdgeDriver();
-		driver.get("https://dashboard.gotowebinar.com/contentLibrary");
+		//WebDriverManager.edgedriver().setup();
+		//WebDriver driver = new EdgeDriver();
+		//driver.get("https://dashboard.gotowebinar.com/contentLibrary");
 		System.out.println("Hello Child Branch");
 		System.out.println("Hello Shammi");
 	}
